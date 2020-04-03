eb20688d201fe7e205000379f7a1cb3549a54ef0
lexto92@gmail.com <lexto92@gmail.com>
2020-04-03 13:38:19 +0300

    add webpuck and web server, dropped main to modules

---
+++ src/components/topRated.js
@@ -0,0 +1,9 @@
+export const getSiteTopRated = () => {
+  return (
+    `<section class="films-list--rated films-list--extra">
+    <h2 class="films-list__title">Top rated</h2>
+    <div class="films-list__container">
+    </div>
+  </section>`
+  );
+};
