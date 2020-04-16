866c31942ec678b45d153f016d5855f59e22c1be
lexto92@gmail.com <lexto92@gmail.com>
2020-04-16 15:15:59 +0300

    realize filter and sort

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,5 +1,5 @@
 export const getFilter = ({name, count}) => {
   return (
-    `<a href="#${name}" class="main-navigation__item main-navigation__item--active">${name}${name === `All movies` ? `` : `<span class="main-navigation__item-count">${count}</span>`}</a ></a > `
+    `<a href="#${name}" class="main-navigation__item" data-filter = ${name}>${name}${name === `All movies` ? `` : `<span class="main-navigation__item-count">${count}</span>`}</a ></a > `
   );
 };
