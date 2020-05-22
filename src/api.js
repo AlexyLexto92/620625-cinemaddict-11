9969d7af8339fdd7d4f86216ac108fd8300d53e6
lexto92@gmail.com <lexto92@gmail.com>
2020-05-22 17:26:23 +0300

    релизовал обновленние данных на сервере/ поправил ошибки

--- src/api.js
+++ src/api.js
@@ -1,10 +1,18 @@
+const STATUS = {
+  OK: 200,
+  REDISRECTION: 300,
+};
+const URL = {
+  movies: `https://11.ecmascript.pages.academy/cinemaddict/movies`,
+};
 const checkStatus = (response) => {
-  if (response.status >= 200 && response.status < 300) {
+  if (response.status >= STATUS.OK && response.status < STATUS.REDISRECTION) {
     return response;
   } else {
     throw new Error(`${response.status}: ${response.statusText}`);
   }
 };
+
 export default class API {
   constructor(authorization) {
     this._authorization = authorization;
@@ -12,7 +20,7 @@ export default class API {
   getMovies() {
     const headers = new Headers();
     headers.append(`Authorization`, this._authorization);
-    return fetch(`https://11.ecmascript.pages.academy/cinemaddict/movies`, {headers})
+    return fetch(URL.movies, {headers})
       .then((response) => response.json());
   }
   getComments(id) {
@@ -23,11 +31,10 @@ export default class API {
   }
 
   _updateMovie(id, data) {
-    debugger
     const headers = new Headers();
     headers.append(`Authorization`, this._authorization);
     headers.append(`Content-Type`, `application/json`);
-    return fetch(`https://11.ecmascript.pages.academy/cinemaddict/movies/${id}`, {
+    return fetch(`${URL.movies + `/` + id }`, {
       method: `PUT`,
       body: JSON.stringify(data),
       headers,
