624e995e99bafcda0c60f6a08853f91e612b7136
lexto92@gmail.com <lexto92@gmail.com>
2020-05-22 13:26:30 +0300

    Подключили API

--- src/main.js
+++ src/main.js
@@ -8,13 +8,13 @@ import {render, RenderPosition} from './components/utils.js';
 import MovieModel from './models/movie.js';
 import CommentModel from './models/comment.js';
 import FilterController from './controller/filters-Controller.js';
-
+import API from './api.js';
+const api = new API();
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 let footer = document.querySelector(`.footer`);
 const movieModel = new MovieModel();
 const commentModel = new CommentModel();
-movieModel.setMovie(dataFilms);
 
 const menu = new Menu();
 const Topfilters = menu.getElement().querySelector(`.main-navigation__items`);
@@ -27,6 +27,12 @@ filterController.render();
 const filmList = new FilmList();
 const pageController = new PageController(filmList, footer, movieModel, commentModel);
 render(siteMainElement, filmList, RenderPosition.BEFOREEND);
-pageController.render();
+
 
 render(footer, new FooterStatistic(dataFilms.length), RenderPosition.BEFOREEND);
+
+api.getTasks()
+  .then((movies) => {
+    movieModel.setMovie(movies);
+    pageController.render();
+  });
